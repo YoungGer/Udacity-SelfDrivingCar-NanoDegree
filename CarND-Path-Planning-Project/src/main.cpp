22a0b9399ad9ddf135f14a67422d95634256455b
YoungGy <771657815@qq.com>
2018-01-23 21:56:06 +0800

    avoid collision with front car

--- CarND-Path-Planning-Project/src/main.cpp
+++ CarND-Path-Planning-Project/src/main.cpp
@@ -245,9 +245,34 @@ int main() {
           	auto sensor_fusion = j[1]["sensor_fusion"];
 
             // Added ------------------------------------------
+            int prev_size = previous_path_x.size();
+
+            // avoid collision ------------------------------------------
+            if (prev_size >0) {
+              car_s = end_path_s;
+            }
+
+            bool too_close = false;
+
+            for (int i = 0; i < sensor_fusion.size(); ++i)
+            {
+              float d = sensor_fusion[i][6];
+              if (d<(2+4*lane+2) && d>(2+4*lane-2) ) {
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx+vy*vy);
+                double check_car_s = sensor_fusion[i][5];
+
+                check_car_s += ((double)prev_size*0.02*check_speed);
+
+                if ((check_car_s>car_s) && ((check_car_s-car_s)<30)) {
+                  ref_vel = 29.5;
+                }
+              }
+            }
 
             // add points  ------------------------------------------
-            int prev_size = previous_path_x.size();
+            
 
             vector<double> ptsx;
             vector<double> ptsy;
