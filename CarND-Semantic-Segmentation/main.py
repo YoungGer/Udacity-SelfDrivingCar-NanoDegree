057b8bd979594406ff3a6d01ed67fec6c09e06c0
YoungGy <771657815@qq.com>
2018-01-26 15:49:49 +0800

    finish sematic segmentation

--- CarND-Semantic-Segmentation/main.py
+++ CarND-Semantic-Segmentation/main.py
@@ -33,7 +33,18 @@ def load_vgg(sess, vgg_path):
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
     
-    return None, None, None, None, None
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = tf.get_default_graph()
+    
+    
+    input_image = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    vgg_layer3_out = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    vgg_layer4_out = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    vgg_layer7_out = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+    
+    return input_image, keep_prob, vgg_layer3_out, vgg_layer4_out, vgg_layer7_out
+
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -46,8 +57,33 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
-    return None
+    # set init and regulizer
+    init = tf.random_normal_initializer(stddev=0.01)
+    regu = tf.contrib.layers.l2_regularizer(1e-3)
+    
+    #  1x1 conv to get same depth
+    pool3 = tf.layers.conv2d(vgg_layer3_out, num_classes, 1, 1, "same", 
+                             kernel_initializer=init ,kernel_regularizer=regu)
+    pool4 = tf.layers.conv2d(vgg_layer4_out, num_classes, 1, 1, "same", 
+                             kernel_initializer=init ,kernel_regularizer=regu)
+    pool5 = tf.layers.conv2d(vgg_layer7_out, num_classes, 1, 1, "same", 
+                             kernel_initializer=init ,kernel_regularizer=regu)
+    
+    # pool5 2x upsample
+    pool5_up2 = tf.layers.conv2d_transpose(pool5, num_classes, 3, 2, "same", 
+                             kernel_initializer=init ,kernel_regularizer=regu)
+    # pool5_up2 + pool4
+    pool45 = tf.add(pool4, pool5_up2)
+    # pool45 2x upsample
+    pool45_up2 = tf.layers.conv2d_transpose(pool45, num_classes, 3, 2, "same", 
+                         kernel_initializer=init ,kernel_regularizer=regu)
+    # pool45_up2 + pool3
+    pool345 = tf.add(pool3, pool45_up2)
+    # pool345 8x upsample
+    pool345_up8 = tf.layers.conv2d_transpose(pool345, num_classes, 16, 8, "same", 
+                         kernel_initializer=init ,kernel_regularizer=regu)
+    
+    return pool345_up8
 tests.test_layers(layers)
 
 
@@ -60,8 +96,13 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
-    return None, None, None
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+    labels = tf.reshape(correct_label, (-1, num_classes))
+    cross_entropy_loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels))
+    train_op = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy_loss)
+
+    return logits, train_op, cross_entropy_loss
+
 tests.test_optimize(optimize)
 
 
@@ -80,15 +121,29 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
-    # TODO: Implement function
-    pass
+    sess.run(tf.global_variables_initializer())
+
+    loss_list = []
+    
+    for e in range(epochs):
+        for img, label, in get_batches_fn(batch_size):
+            feed_dict={input_image: img,
+                      correct_label: label,
+                      keep_prob: .5,
+                      learning_rate: 1e-4}
+            _, loss = sess.run([train_op, cross_entropy_loss], feed_dict=feed_dict)
+            loss_list.append(loss)
+            print('epoch %i, loss: %f'%(e, loss))
+    return loss_list
+
 tests.test_train_nn(train_nn)
 
 
 def run():
     num_classes = 2
     image_shape = (160, 576)
-    data_dir = './data'
+    #data_dir = './data'
+    data_dir = '/home/guangyaoyang/data'
     runs_dir = './runs'
     tests.test_for_kitti_dataset(data_dir)
 
@@ -99,7 +154,13 @@ def run():
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
 
-    with tf.Session() as sess:
+    epochs = 7
+    batch_size = 5
+
+        
+    with tf.Session(graph=tf.Graph()) as sess:
+        
+        
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
         # Create function to get batches
@@ -109,13 +170,28 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
-
+        input_image, keep_prob, vgg_layer3_out, vgg_layer4_out, vgg_layer7_out = load_vgg(sess, vgg_path)
+        nn_last_layer = layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes)
+        correct_label = tf.placeholder(tf.int32, shape=[None, None, None, num_classes])
+        learning_rate = tf.placeholder(tf.float32)
+        logits, train_op, cross_entropy_loss = optimize(nn_last_layer, correct_label, learning_rate, num_classes)
+        
+        # to save the trained model (preparation)
+        # saver = tf.train.Saver()
+        
         # TODO: Train NN using the train_nn function
+        train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_loss, input_image,
+                 correct_label, keep_prob, learning_rate)
+        
 
         # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
 
         # OPTIONAL: Apply the trained model to a video
+
+
+        # # save model
+        #saver.save(sess, './runs/sem_seg_model.ckpt')
 
 
 if __name__ == '__main__':
